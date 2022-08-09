0aa82624ce5ae83a71e38f3a6f03a79e7d31f1bb
Daniel Thoma <dt@clickbar.dev>
2022-08-09 18:44:53 +0200

    update schemma grammar and blueprint builder

---
+++ src/Schema/Grammars/MagellanGrammar.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Clickbar\Magellan\Schema\Grammars;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Fluent;
+
+/**
+ * @package Clickbar\Magellan\Schema\Grammars
+ * @mixin \Illuminate\Database\Schema\Grammars\PostgresGrammar
+ */
+class MagellanGrammar
+{
+
+    /*
+     *  Types
+     */
+
+    public function typePoint(): \Closure
+    {
+        return function (Fluent $column) {
+
+            return MagellanGrammarHelper::createTypeDefinition($column, 'POINT');
+        };
+    }
+
+    public function typePointZ(): \Closure
+    {
+        return function (Fluent $column) {
+            return MagellanGrammarHelper::createTypeDefinition($column, 'POINTZ');
+        };
+
+    }
+
+    public function typeMultiPoint(): \Closure
+    {
+        return function (Fluent $column) {
+            return MagellanGrammarHelper::createTypeDefinition($column, 'MULTIPOINT');
+        };
+    }
+
+    public function typePolygon(): \Closure
+    {
+        return function (Fluent $column) {
+            return MagellanGrammarHelper::createTypeDefinition($column, 'POLYGON');
+        };
+    }
+
+    public function typePolygonZ(): \Closure
+    {
+        return function (Fluent $column) {
+            return MagellanGrammarHelper::createTypeDefinition($column, 'POLYGONZ');
+        };
+    }
+
+    public function typeMultiPolygon(): \Closure
+    {
+        return function (Fluent $column) {
+            return MagellanGrammarHelper::createTypeDefinition($column, 'MULTIPOLYGON');
+        };
+    }
+
+    public function typeMultiPolygonZ(): \Closure
+    {
+        return function (Fluent $column) {
+            return MagellanGrammarHelper::createTypeDefinition($column, 'MULTIPOLYGONZ');
+        };
+    }
+
+    public function typeLinestring(): \Closure
+    {
+        return function (Fluent $column) {
+            return MagellanGrammarHelper::createTypeDefinition($column, 'LINESTRING');
+        };
+    }
+
+    public function typeLinestringZ(): \Closure
+    {
+        return function (Fluent $column) {
+            return MagellanGrammarHelper::createTypeDefinition($column, 'LINESTRINGZ');
+        };
+    }
+
+    public function typeMultiLinestring(): \Closure
+    {
+        return function (Fluent $column) {
+            return MagellanGrammarHelper::createTypeDefinition($column, 'MULTILINESTRING');
+        };
+    }
+
+    public function typeMultiLinestringZ(): \Closure
+    {
+        return function (Fluent $column) {
+            return MagellanGrammarHelper::createTypeDefinition($column, 'MULTILINESTRINGZ');
+        };
+    }
+
+    public function typeGeography(): \Closure
+    {
+        return function (Fluent $column) {
+            return MagellanGrammarHelper::createTypeDefinition($column, 'GEOGRAPHY');
+        };
+    }
+
+    public function typeGeometry(): \Closure
+    {
+        return function (Fluent $column) {
+            return MagellanGrammarHelper::createTypeDefinition($column, 'GEOMETRY');
+        };
+    }
+
+    public function typeGeometrycollection(): \Closure
+    {
+        return function (Fluent $column) {
+            return MagellanGrammarHelper::createTypeDefinition($column, 'GEOMETRYCOLLECTION');
+        };
+    }
+
+    /*
+     *  COMPILE Statements
+     */
+
+    /**
+     * Adds a statement to add a geometrycollection geometry column
+     *
+     * @return \Closure
+     */
+    public function compileGeometrycollection(): \Closure
+    {
+        return function (Blueprint $blueprint, Fluent $command) {
+            $command->type = 'GEOMETRYCOLLECTION';
+
+            return $this->compileGeometry($blueprint, $command);
+        };
+    }
+
+    /**
+     * Adds a statement to create the postgis extension
+     *
+     * @return \Closure
+     */
+    public function compileEnablePostgis(): \Closure
+    {
+        return function () {
+            return 'CREATE EXTENSION postgis;';
+        };
+    }
+
+    /**
+     * Adds a statement to create the postgis extension, if it doesn't already exist
+     *
+     * @return \Closure
+     */
+    public function compileEnablePostgisIfNotExists(): \Closure
+    {
+        return function () {
+            return 'CREATE EXTENSION IF NOT EXISTS postgis;';
+        };
+    }
+
+    /**
+     * Adds a statement to drop the postgis extension
+     *
+     * @return \Closure
+     */
+    public function compileDisablePostgis(): \Closure
+    {
+        return function () {
+            return 'DROP EXTENSION postgis;';
+        };
+    }
+
+    /**
+     * Adds a statement to drop the postgis extension, if it exists
+     *
+     * @return \Closure
+     */
+    public function compileDisablePostgisIfExists(): \Closure
+    {
+        return function () {
+            return 'DROP EXTENSION IF EXISTS postgis;';
+        };
+    }
+}
---
+++ src/Schema/MagellanBlueprint.php
@@ -0,0 +1,248 @@
+<?php
+
+namespace Clickbar\Magellan\Schema;
+
+use Illuminate\Database\Schema\ColumnDefinition;
+use Illuminate\Support\Fluent;
+
+/**
+ * @package Clickbar\Magellan\Schema
+ * @mixin \Illuminate\Database\Schema\Blueprint
+ */
+class MagellanBlueprint
+{
+    /**
+     * Add a point column on the table
+     *
+     * @param      $column
+     * @param int|null $srid
+     * @param string $postgisType
+     * @return ColumnDefinition
+     */
+    public function magellanPoint(): \Closure
+    {
+        return function ($column, $srid = 4326, string $postgisType = 'GEOGRAPHY') {
+            return $this->addColumn('point', $column, compact('postgisType', 'srid'));
+        };
+    }
+
+    /**
+     * Add a point column on the table
+     *
+     * @param      $column
+     * @param int|null $srid
+     * @param string $postgisType
+     * @return ColumnDefinition
+     */
+    public function magellanPointz(): \Closure
+    {
+        return function ($column, $srid = 4326, string $postgisType = 'GEOGRAPHY') {
+            return $this->addColumn('pointz', $column, compact('postgisType', 'srid'));
+        };
+    }
+
+    /**
+     * Add a multipoint column on the table
+     *
+     * @param      $column
+     * @param int|null $srid
+     * @param string $postgisType
+     * @return ColumnDefinition
+     */
+    public function magellanMultipoint(): \Closure
+    {
+        return function ($column, $srid = 4326, string $postgisType = 'GEOGRAPHY') {
+            return $this->addColumn('multipoint', $column, compact('postgisType', 'srid'));
+        };
+    }
+
+    /**
+     * Add a polygon column on the table
+     *
+     * @param      $column
+     * @param int|null $srid
+     * @param string $postgisType
+     * @return ColumnDefinition
+     */
+    public function magellanPolygon(): \Closure
+    {
+        return function ($column, $srid = 4326, string $postgisType = 'GEOGRAPHY') {
+            return $this->addColumn('polygon', $column, compact('postgisType', 'srid'));
+        };
+    }
+
+    /**
+     * Add a multipolygon column on the table
+     *
+     * @param      $column
+     * @param int|null $srid
+     * @param string $postgisType
+     * @return ColumnDefinition
+     */
+    public function magellanMultipolygon(): \Closure
+    {
+        return function ($column, $srid = 4326, string $postgisType = 'GEOGRAPHY') {
+            return $this->addColumn('multipolygon', $column, compact('postgisType', 'srid'));
+        };
+    }
+
+    /**
+     * Add a multipolygonz column on the table
+     *
+     * @param $column
+     * @param int|null $srid
+     * @param string $postgisType
+     * @return ColumnDefinition
+     */
+    public function magellanMultipolygonz(): \Closure
+    {
+        return function ($column, $srid = 4326, string $postgisType = 'GEOGRAPHY') {
+            return $this->addColumn('multipolygonz', $column, compact('postgisType', 'srid'));
+        };
+    }
+
+    /**
+     * Add a linestring column on the table
+     *
+     * @param      $column
+     * @param int|null $srid
+     * @param string $postgisType
+     * @return ColumnDefinition
+     */
+    public function magellanLinestring(): \Closure
+    {
+        return function ($column, $srid = 4326, string $postgisType = 'GEOGRAPHY') {
+            return $this->addColumn('linestring', $column, compact('postgisType', 'srid'));
+        };
+    }
+
+    /**
+     * Add a linestringz column on the table
+     *
+     * @param      $column
+     * @param int|null $srid
+     * @param string $postgisType
+     * @return ColumnDefinition
+     */
+    public function magellanLinestringz(): \Closure
+    {
+        return function ($column, $srid = 4326, string $postgisType = 'GEOGRAPHY') {
+            return $this->addColumn('linestringz', $column, compact('postgisType', 'srid'));
+        };
+    }
+
+    /**
+     * Add a multilinestring column on the table
+     *
+     * @param      $column
+     * @param int|null $srid
+     * @param string $postgisType
+     * @return ColumnDefinition
+     */
+    public function magellanMultilinestring(): \Closure
+    {
+        return function ($column, $srid = 4326, string $postgisType = 'GEOGRAPHY') {
+            return $this->addColumn('multilinestring', $column, compact('postgisType', 'srid'));
+        };
+    }
+
+    /**
+     * Add a geography column on the table
+     *
+     * @param string $column
+     * @param int|null $srid
+     * @param string $postgisType
+     * @return ColumnDefinition
+     */
+    public function magellanGeography(): \Closure
+    {
+        return function ($column, $srid = 4326, string $postgisType = 'GEOGRAPHY') {
+            return $this->addColumn('geography', $column, compact('postgisType', 'srid'));
+        };
+    }
+
+    /**
+     * Add a geometry column on the table
+     *
+     * @param string $column
+     * @param int|null $srid
+     * @param string $postgisType
+     * @return ColumnDefinition
+     */
+    public function magellanGeometry(): \Closure
+    {
+        return function ($column, $srid = 4326, string $postgisType = 'GEOGRAPHY') {
+            return $this->addColumn('geometry', $column, compact('postgisType', 'srid'));
+        };
+    }
+
+    /**
+     * Add a geometrycollection column on the table
+     *
+     * @param      $column
+     * @param null $srid
+     * @param int $dimensions
+     * @param bool $typmod
+     * @return Fluent
+     */
+    public function magellanGeometrycollection(): \Closure
+    {
+        return function ($column, $srid = 4326) {
+            $postgisType = 'GEOMETRY';
+
+            return $this->addColumn('geometrycollection', $column, compact('postgisType', 'srid'));
+        };
+    }
+
+    /**
+     * Enable postgis on this database.
+     * Will create the extension in the database.
+     *
+     * @return Fluent
+     */
+    public function magellanEnablePostgis(): \Closure
+    {
+        return function () {
+            return $this->addCommand('enablePostgis');
+        };
+    }
+
+    /**
+     * Enable postgis on this database.
+     * Will create the extension in the database if it doesn't already exist.
+     *
+     * @return Fluent
+     */
+    public function magellanEnablePostgisIfNotExists(): \Closure
+    {
+        return function () {
+        return $this->addCommand('enablePostgisIfNotExists');
+        };
+    }
+
+    /**
+     * Disable postgis on this database.
+     * WIll drop the extension in the database.
+     *
+     * @return Fluent
+     */
+    public function magellanDisablePostgis(): \Closure
+    {
+        return function () {
+            return $this->addCommand('disablePostgis');
+        };
+    }
+
+    /**
+     * Disable postgis on this database.
+     * WIll drop the extension in the database if it exists.
+     *
+     * @return Fluent
+     */
+    public function magellanDisablePostgisIfExists(): \Closure
+    {
+        return function () {
+            return $this->addCommand('disablePostgisIfExists');
+        };
+    }
+}
