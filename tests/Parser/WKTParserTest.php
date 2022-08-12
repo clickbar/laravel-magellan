3d1d17344868ad6c0299a95fed1b6efc534303ee
Adrian <ah@clickbar.dev>
2022-08-12 15:42:50 +0200

    Removed empty test file

--- tests/Parser/WKTParserTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Clickbar\Magellan\Geometries\GeometryFactory;
-use Clickbar\Magellan\IO\Parser\WKT\WKTParser;
-
-beforeEach(function () {
-    $this->parser = new WKTParser(new GeometryFactory());
-});
