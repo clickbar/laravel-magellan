f30cda22890c3a16e36280ae49d4634b36651a05
Tobias Kröll <tk@clickbar.dev>
2023-01-03 14:41:38 +0100

    Add tests for empty point wkb parsing

--- tests/Parser/WKB/WKBParserPointTest.php
+++ tests/Parser/WKB/WKBParserPointTest.php
@@ -19,6 +19,19 @@ test('can parse 2D WKB Point', function () {
     expect($point->getY())->toBe(50.12345);
 })->group('WKB Point');
 
+test('can parse 2D EMPTY WKB Point', function () {
+    $pointWKB = '0101000000000000000000F87F000000000000F87F';
+
+    $point = $this->parser->parse($pointWKB);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_2D);
+    expect($point->isEmpty())->toBeTrue();
+    expect($point->getZ())->toBe(null);
+    expect($point->getM())->toBe(null);
+    expect($point)->geometryHasSrid(null);
+})->group('WKB Point');
+
 test('can parse 2D WKB Point with SRID', function () {
     $pointWKB = '0101000020E6100000E561A1D6343F20407958A835CD0F4940'; // st_setsrid(st_makepoint(8.12345, 50.12345), 4326)
 
@@ -27,6 +40,19 @@ test('can parse 2D WKB Point with SRID', function () {
     expect($point)->toBeInstanceOf(Point::class);
     expect($point->getX())->toBe(8.12345);
     expect($point->getY())->toBe(50.12345);
+    expect($point)->geometryHasSrid(4326);
+})->group('WKB Point');
+
+test('can parse 2D EMPTY WKB Point with SRID', function () {
+    $pointWKB = '0101000020e6100000000000000000f87f000000000000f87f';
+
+    $point = $this->parser->parse($pointWKB);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_2D);
+    expect($point->isEmpty())->toBeTrue();
+    expect($point->getZ())->toBe(null);
+    expect($point->getM())->toBe(null);
     expect($point)->geometryHasSrid(4326);
 })->group('WKB Point');
 
@@ -41,6 +67,19 @@ test('can parse 3DZ WKB Point', function () {
     expect($point->getZ())->toBe(10.0);
 })->group('WKB Point');
 
+test('can parse 3DZ EMPTY WKB Point', function () {
+    $pointWKB = '0101000080000000000000F87F000000000000F87F000000000000F87F';
+
+    $point = $this->parser->parse($pointWKB);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_3DZ);
+    expect($point->isEmpty())->toBeTrue();
+    expect($point->getZ())->toBeNan();
+    expect($point->getM())->toBe(null);
+    expect($point)->geometryHasSrid(null);
+})->group('WKB Point');
+
 test('can parse 3DZ WKB Point with SRID', function () {
     $pointWKB = '01010000A0E6100000E561A1D6343F20407958A835CD0F49400000000000002440'; // st_setsrid(st_makepoint(8.12345, 50.12345, 10), 4326)
 
@@ -50,6 +89,19 @@ test('can parse 3DZ WKB Point with SRID', function () {
     expect($point->getX())->toBe(8.12345);
     expect($point->getY())->toBe(50.12345);
     expect($point->getZ())->toBe(10.0);
+    expect($point)->geometryHasSrid(4326);
+})->group('WKB Point');
+
+test('can parse 3DZ EMPTY WKB Point with SRID', function () {
+    $pointWKB = '01010000A0E6100000000000000000F87F000000000000F87F000000000000F87F';
+
+    $point = $this->parser->parse($pointWKB);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_3DZ);
+    expect($point->isEmpty())->toBeTrue();
+    expect($point->getZ())->toBeNan();
+    expect($point->getM())->toBe(null);
     expect($point)->geometryHasSrid(4326);
 })->group('WKB Point');
 
@@ -65,6 +117,19 @@ test('can parse 3DM WKB Point', function () {
     expect($point->getM())->toBe(10.0);
 })->group('WKB Point');
 
+test('can parse 3DM EMPTY WKB Point', function () {
+    $pointWKB = '0101000040000000000000F87F000000000000F87F000000000000F87F';
+
+    $point = $this->parser->parse($pointWKB);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_3DM);
+    expect($point->isEmpty())->toBeTrue();
+    expect($point->getZ())->toBe(null);
+    expect($point->getM())->toBeNan();
+    expect($point)->geometryHasSrid(null);
+})->group('WKB Point');
+
 test('can parse 3DM WKB Point with SRID', function () {
     $pointWKB = '0101000060E6100000E561A1D6343F20407958A835CD0F49400000000000002440'; // st_setsrid(ST_MakePointM(8.12345, 50.12345,10), 4326)
 
@@ -76,6 +141,19 @@ test('can parse 3DM WKB Point with SRID', function () {
     expect($point->getX())->toBe(8.12345);
     expect($point->getY())->toBe(50.12345);
     expect($point->getM())->toBe(10.0);
+})->group('WKB Point');
+
+test('can parse 3DM EMPTY WKB Point with SRID', function () {
+    $pointWKB = '0101000060E6100000000000000000F87F000000000000F87F000000000000F87F';
+
+    $point = $this->parser->parse($pointWKB);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_3DM);
+    expect($point->isEmpty())->toBeTrue();
+    expect($point->getZ())->toBe(null);
+    expect($point->getM())->toBeNan();
+    expect($point)->geometryHasSrid(4326);
 })->group('WKB Point');
 
 test('can parse 4D WKB Point', function () {
@@ -91,6 +169,19 @@ test('can parse 4D WKB Point', function () {
     expect($point->getM())->toBe(20.0);
 })->group('WKB Point');
 
+test('can parse 4D EMPTY WKB Point', function () {
+    $pointWKB = '01010000C0000000000000F87F000000000000F87F000000000000F87F000000000000F87F';
+
+    $point = $this->parser->parse($pointWKB);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_4D);
+    expect($point->isEmpty())->toBeTrue();
+    expect($point->getZ())->toBeNan();
+    expect($point->getM())->toBeNan();
+    expect($point)->geometryHasSrid(null);
+})->group('WKB Point');
+
 test('can parse 4D WKB Point with SRID', function () {
     $pointWKB = '01010000E0E6100000E561A1D6343F20407958A835CD0F494000000000000024400000000000003440'; // st_setsrid(st_makepoint(8.12345, 50.12345, 10, 20), 4326)
 
@@ -102,5 +193,18 @@ test('can parse 4D WKB Point with SRID', function () {
     expect($point->getY())->toBe(50.12345);
     expect($point->getZ())->toBe(10.0);
     expect($point->getM())->toBe(20.0);
+    expect($point)->geometryHasSrid(4326);
+})->group('WKB Point');
+
+test('can parse 4D EMPTY WKB Point with SRID', function () {
+    $pointWKB = '01010000E0E6100000000000000000F87F000000000000F87F000000000000F87F000000000000F87F';
+
+    $point = $this->parser->parse($pointWKB);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_4D);
+    expect($point->isEmpty())->toBeTrue();
+    expect($point->getZ())->toBeNan();
+    expect($point->getM())->toBeNan();
     expect($point)->geometryHasSrid(4326);
 })->group('WKB Point');
