0f89671ef4646b67fc489a31a54cc1b2efb5deef
Adrian <ah@clickbar.dev>
2023-01-03 16:17:09 +0100

    Added WKT Parser Tests for empty Point

--- tests/Parser/WKT/WKTParserPointTest.php
+++ tests/Parser/WKT/WKTParserPointTest.php
@@ -9,6 +9,94 @@ beforeEach(function () {
     $this->parser = App::make(WKTParser::class);
 });
 
+test('can parse empty 2D WKT Point', function () {
+    $pointWKT = 'POINT EMPTY';
+
+    $point = $this->parser->parse($pointWKT);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_2D);
+    expect($point)->geometryHasSrid(null);
+    expect($point->isEmpty())->toBeTrue();
+})->group('WKT Point');
+
+test('can parse empty 3DZ WKT Point', function () {
+    $pointWKT = 'POINT Z EMPTY';
+
+    $point = $this->parser->parse($pointWKT);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_3DZ);
+    expect($point)->geometryHasSrid(null);
+    expect($point->isEmpty())->toBeTrue();
+})->group('WKT Point');
+
+test('can parse empty 3DM WKT Point', function () {
+    $pointWKT = 'POINT M EMPTY';
+
+    $point = $this->parser->parse($pointWKT);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_3DM);
+    expect($point)->geometryHasSrid(null);
+    expect($point->isEmpty())->toBeTrue();
+})->group('WKT Point');
+
+test('can parse empty 4D WKT Point', function () {
+    $pointWKT = 'POINT ZM EMPTY';
+
+    $point = $this->parser->parse($pointWKT);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_4D);
+    expect($point)->geometryHasSrid(null);
+    expect($point->isEmpty())->toBeTrue();
+})->group('WKT Point');
+
+test('can parse empty 2D WKT Point with SRID', function () {
+    $pointWKT = 'SRID=4326;POINT EMPTY';
+
+    $point = $this->parser->parse($pointWKT);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_2D);
+    expect($point)->geometryHasSrid(4326);
+    expect($point->isEmpty())->toBeTrue();
+})->group('WKT Point');
+
+test('can parse empty 3DZ WKT Point with SRID', function () {
+    $pointWKT = 'SRID=4326;POINT Z EMPTY';
+
+    $point = $this->parser->parse($pointWKT);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_3DZ);
+    expect($point)->geometryHasSrid(4326);
+    expect($point->isEmpty())->toBeTrue();
+})->group('WKT Point');
+
+test('can parse empty 3DM WKT Point with SRID', function () {
+    $pointWKT = 'SRID=4326;POINT M EMPTY';
+
+    $point = $this->parser->parse($pointWKT);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_3DM);
+    expect($point)->geometryHasSrid(4326);
+    expect($point->isEmpty())->toBeTrue();
+})->group('WKT Point');
+
+test('can parse empty 4D WKT Point with SRID', function () {
+    $pointWKT = 'SRID=4326;POINT ZM EMPTY';
+
+    $point = $this->parser->parse($pointWKT);
+
+    expect($point)->toBeInstanceOf(Point::class);
+    expect($point)->geometryHasDimension(Dimension::DIMENSION_4D);
+    expect($point)->geometryHasSrid(4326);
+    expect($point->isEmpty())->toBeTrue();
+})->group('WKT Point');
+
 test('can parse 2D WKT Point', function () {
     $pointWKT = 'POINT(8.12345 50.12345)';
 
