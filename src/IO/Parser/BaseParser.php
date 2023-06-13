9e73f8f6a93b1b8814d1a85b9662ac67e4525ed9
Connor Tumbleson <connor@sourcetoad.com>
2023-06-13 16:45:35 -0400

    fix: base parser loads Feature factory

--- src/IO/Parser/BaseParser.php
+++ src/IO/Parser/BaseParser.php
@@ -3,12 +3,14 @@
 namespace Clickbar\Magellan\IO\Parser;
 
 use Clickbar\Magellan\Data\Geometries\Geometry;
+use Clickbar\Magellan\IO\FeatureModelFactory;
 use Clickbar\Magellan\IO\GeometryModelFactory;
 
 abstract class BaseParser
 {
     public function __construct(
-        protected GeometryModelFactory $factory
+        protected GeometryModelFactory $factory,
+        protected ?FeatureModelFactory $featureFactory = null,
     ) {
     }
 
