bd485f69b99165a839d1f085f47b08ce5c1e9fa7
Tobias Kröll <tk@clickbar.dev>
2024-12-27 16:40:57 +0100

    fix(caster): correctly use SQL generator & fix type annotations

--- src/Cast/GeometryCast.php
+++ src/Cast/GeometryCast.php
@@ -3,43 +3,44 @@
 namespace Clickbar\Magellan\Cast;
 
 use Clickbar\Magellan\Data\Geometries\Geometry;
-use Clickbar\Magellan\IO\Generator\WKB\WKBGenerator;
+use Clickbar\Magellan\IO\Generator\BaseGenerator;
+use Clickbar\Magellan\IO\Generator\WKT\WKTGenerator;
 use Clickbar\Magellan\IO\Parser\WKB\WKBParser;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\App;
 
+/**
+ * @implements CastsAttributes<Geometry,Geometry>
+ */
 class GeometryCast implements CastsAttributes
 {
     protected WKBParser $wkbParser;
 
-    protected WKBGenerator $sqlGenerator;
+    protected BaseGenerator $sqlGenerator;
 
     public function __construct()
     {
         $this->wkbParser = App::make(WKBParser::class);
-        $this->sqlGenerator = App::make(WKBGenerator::class);
+
+        $generatorClass = config('magellan.sql_generator', WKTGenerator::class);
+        $this->sqlGenerator = new $generatorClass;
     }
 
     /**
      * Cast the given value.
      *
-     * @param  \Illuminate\Database\Eloquent\Model  $model
-     * @return \Clickbar\Magellan\Data\Geometries\Geometry|null
+     * @param  Model  $model
      */
     public function get($model, string $key, mixed $value, array $attributes)
     {
-        // The geometry might be already casted on the lowest level if the alias hits an postgis field name
-        if ($value instanceof Geometry) {
-            return $value;
-        }
-
         return isset($value) ? $this->wkbParser->parse($value) : null;
     }
 
     /**
      * Prepare the given value for storage.
      *
-     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  Model  $model
      */
     public function set($model, string $key, mixed $value, array $attributes)
     {
