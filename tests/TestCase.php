b491b5a3af1d29020e5d0d00c3ab53a85041ae9f
Tobias Kröll <tk@clickbar.dev>
2026-05-11 12:17:25 +0200

    feat: support Laravel 13
    Added constraints to allow Laravel 13.
    
    Fixed visibility of `latestResponse` in TestCase, to account for differences in newer versions of Testbench.
    
    Removed phpunit requirement as it is implicitly installed because of pest.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,6 +9,8 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    public static $latestResponse;
+
     protected function setUp(): void
     {
         parent::setUp();
